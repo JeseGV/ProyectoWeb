20e2c67ef56fa53daa35dc407d428734c29e5f8c
IsEdgar26 <guangorena12345@gmail.com>
2022-11-29 21:36:43 -0700

    agregar

---
+++ agregar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="style-musica.css" />
+    <header align="center">
+      <h1 class="head"><tt>CONTACTOS</tt></h1>
+    </header>
+    <title>Contactos</title>
+  </head>
+  <body class="infogra">
+    <div>
+      <aside>
+        <ul>
+          <li><h2><a href="index.php">Contactos</a></h2></li>
+          <li><h2><a href="agregar.php">Agregar</a></h2></li>
+          <li><h2><a href="eliminar.php">Eliminar</a></h2></li>
+          <li><h2><a href="editar.php">Editar</a></h2></li>
+        </ul>
+      </aside>
+      <div class="contenido">
+        <section>
+          <div class="titulo">
+          </div>
+          <div class="texto">
+            <form action="agregarsql.php" method="POST">
+                <table>
+                <tr><td>Nombre</td><td><input type="text" name"nombres"></td></tr>
+			    <tr><td>Número</td><td><input type="tel" name"telefono"></td></tr>
+			    <tr><td><input type="submit"></td><td><input type="reset"></td></tr>
+                </table>
+            </form>   
+          </div>
+        </section>
+    </div>
+  </body>
+</html>

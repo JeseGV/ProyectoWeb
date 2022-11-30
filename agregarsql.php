0e68ca8e70a04a2bbd3c44b1f92451b642942e13
JeseGV <d.villegas1579@gmail.com>
2022-11-29 21:57:56 -0600

    aver

--- agregarsql.php
+++ agregarsql.php
@@ -1,7 +1,14 @@
+<!--
 <?php
 	$mysqli = new mysqli('localhost', 'root', '', 'contactos');
 	$mysqli->set_charset("utf8");
 	$query = $mysqli->query("INSERT INTO personas (id_Contacto, Nombre, Número)
      VALUES (NULL, '".$_POST['nombre']."', '".$_POST['telefono']."');");
 	header("Location: index.php");
+?>
+-->
+<?php 
+$mysqli = new mysqli('localhost', 'root', '', 'contactos');
+$query = $mysqli->query("INSERT INTO personas (id_contacto, Nombres, Número) VALUES (NULL, '".$_POST['nombres']."', '".$_POST['telefono']."');");
+header("Location: index.php");
 ?>

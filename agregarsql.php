c866c5a0fbc9da724a6cd3846482a696b212d199
IsEdgar26 <guangorena12345@gmail.com>
2022-11-29 21:41:17 -0700

    acento

--- agregarsql.php
+++ agregarsql.php
@@ -1,7 +1,7 @@
 <?php
 	$mysqli = new mysqli('localhost', 'root', '', 'contactos');
 	$mysqli->set_charset("utf8");
-	$query = $mysqli->query("INSERT INTO datos (id_Contacto, Nombre, Numero)
+	$query = $mysqli->query("INSERT INTO datos (id_Contacto, Nombre, Número)
      VALUES (NULL, '".$_POST['nombre']."', '".$_POST['telefono']."');");
 	header("Location: index.php");
 ?>

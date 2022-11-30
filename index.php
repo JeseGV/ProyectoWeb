f551bda38cc6849b992bcdd1e61dc2e71262abe2
IsEdgar26 <guangorena12345@gmail.com>
2022-11-29 20:44:44 -0700

    p

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" type="text/css" href="style-musica.css" />
+    <header align="center">
+      <h1 class="head"><tt>INFOGRAFIAS</tt></h1>
+    </header>
+    <title>INFOGRAFIAS</title>
+  </head>
+  <body class="infogra">
+    <div>
+      <aside>
+        <ul>
+          <li><a href="index.html">Inicio</a></li>
+          <li><a href="#a1">Muse</a></li>
+          <li><a href="#a2">twenty one pilots</a></li>
+          <li><a href="#a3">Gorillaz</a></li>
+          <li><a href="#a4"> imagine dragons</a></li>
+          <li><a href="#a5">Royale blood</a></li>
+          <li><a href="#a6">Michael Jackson</a></li>
+          <li><a href="#a7">Coldplay</a></li>
+          <li><a href="#a8">Lady Gaga</a></li>
+          <li><a href="#a9">The weeknd</a></li>
+          <li><a href="#a10">Bad bunny</a></li>
+          <li><a href="#a11">JBalvin</a></li>
+        </ul>
+      </aside>
+      <div class="contenido">
+        <section>
+          <div class="titulo">
+            <h2>
+              <a name="a1">Muse</a>
+            </h2>
+          </div>
+          <div class="texto">
+            <p>
+              Muse es una banda de rock británica formada en 1994, en
+              Teignmouth, Devon. Desde su formación, sus integrantes son: Matt
+              Bellamy (voz, guitarra, teclados), Christopher Wolstenholme (bajo,
+              coros) y Dominic Howard (batería).<br /><br />
+              Tras el lanzamiento de Black Holes and Revelations, Morgan
+              Nicholls comenzó a colaborar con la banda durante las
+              presentaciones en vivo, haciéndose cargo de teclados, samples, de
+              algunos coros, rara vez del bajo y últimamente de la segunda
+              guitarra.​ La banda es conocida por sus extravagantes espectáculos
+              en vivo, por fusionar géneros musicales como el rock alternativo,
+              rock espacial, rock progresivo, rock sinfónico y electrónica,​
+              además por los atípicos intereses de Bellamy en la conspiración
+              global, la revolución, la astrofísica, vida extraterrestre, los
+              fantasmas, la teología y el apocalipsis; temas que se ven
+              reflejados en sus letras.
+            </p>
+          </div>
+        </section>
+        </div>
+  </body>
+</html>

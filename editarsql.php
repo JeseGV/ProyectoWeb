68dd91faf5674c43985f6174f092b34339cf9a4b
IsEdgar26 <guangorena12345@gmail.com>
2022-11-29 23:52:14 -0700

    y

--- editarsql.php
+++ editarsql.php
@@ -2,6 +2,6 @@
 	$mysqli = new mysqli('localhost', 'root', '', 'contactos');
 	$mysqli->set_charset("utf8");
 	$query = $mysqli->query("UPDATE personas SET Nombre='".$_POST['nombre']."', 
-	Teléfono='".$_POST['telefono']."' WHERE id_Contacto='".$_POST['idcontacto']."'");
+	Número='".$_POST['telefono']."' WHERE id_Contacto='".$_POST['idcontacto']."'");
 	header("location: cambios.php");
 ?>
